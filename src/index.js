d6d9acc3a008fa8c206b5e7e539e70be727bbbfb
PogudoTanya <34543226+PogudoTanya@users.noreply.github.com>
2019-03-18 04:01:38 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+   <input id='userInput' size=30>
+    <button onClick='userSubmit()'>Translate</button>
+    <div id='result'></div>
+  
+    <script type='text/javascript'>
+        function userSubmit() {
+            var chislo=document.getElementById('userInput').value;
+            var res= (chislo * 9/5) + 32;
+            document.getElementById('result').innerHTML='Celsius to Fahrenheit conversion is: '+ res;
+        }
+
+        
+    </script>
 };
